9125cb22c15eb66619a57fc22222db35f848e6aa
JimmyKmi <jimmy@lzj.ac.cn>
2025-03-16 20:46:45 +0800

    refactor(next-config): update configuration and add server runtime settings
    - Remove unused NEXTAUTH_URL environment variable
    - Add assetPrefix configuration for dynamic site host- Introduce serverRuntimeConfig with hostname and port settings
    - Simplify and reorganize existing configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    webpackBuildWorker: true
-  },
-  output: 'standalone',
-  webpack: (config) => {
-    config.externals = [...(config.externals || []), 'crypto', 'node:crypto']
-    return config
-  },
-  env: {
-    // 环境变量由lib/env.ts统一管理
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  }
+    eslint: {ignoreDuringBuilds: true},
+    typescript: {ignoreBuildErrors: true},
+    images: {unoptimized: true},
+    experimental: {webpackBuildWorker: true},
+    output: 'standalone',
+    assetPrefix: process.env.SITE_HOST || undefined,
+    webpack: (config) => {
+        config.externals = [...(config.externals || []), 'crypto', 'node:crypto']
+        return config
+    },
+    env: {
+        // 环境变量由lib/env.ts统一管理
+        // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    },
+    serverRuntimeConfig: {
+        hostname: '0.0.0.0',
+        port: 3000
+    }
 }
 
 export default nextConfig
